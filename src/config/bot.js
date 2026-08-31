b71f51337fe46cb2a50121dbf8627ad500cb20c0
Rbwwwwwwwww <rbwwwwwwwwwwwwwwwwwwww@atomicmail.io>
2026-09-01 00:53:59 +0330

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -21,7 +21,7 @@ export const botConfig = {
     // 3 = Watching
     // 4 = Custom
     // 5 = Competing
-    activities: [
+    activities: [0
       {
         name: "Custom Status", // required by Discord API, not shown in the client
         state: "Hello",     // this is what people actually see
